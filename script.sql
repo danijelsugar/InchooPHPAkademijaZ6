d815a249457ffb7942e35e753a337f412c3812aa
Danijel Šugar <danijel.sugar@outlook.com>
2019-02-02 16:10:37 +0100

    New row in database for time when post is posted

--- script.sql
+++ script.sql
@@ -4,7 +4,8 @@ use social_network;
 
 create table post(
 id int not null primary key auto_increment,
-content text
+content text,
+posttime DATETIME DEFAULT CURRENT_TIMESTAMP
 )engine=InnoDB;
 
 insert into post (content) values ('Evo danas pada kiša opet :('), ('Jedem jagode.');
